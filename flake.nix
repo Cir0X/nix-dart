0f367f5e9dfd9528534aaa7418de20a7ecec273b
Wolfhard Prell <wolfhard.prell@gmail.com>
2022-07-26 19:50:48 +0200

    use my pub2nix fork

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     pub2nix = {
-      url = "github:paulyoung/pub2nix";
+      url = "github:cir0x/pub2nix";
       flake = false;
     };
   };
